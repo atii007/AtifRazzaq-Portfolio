a6eb7ae1964305d2384b69e759481c5ec2833596
Atif Razzaq <120604563+atii007@users.noreply.github.com>
2024-05-27 17:22:01 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/src/components/navbar/Navbar";
-import Themes from "@/src/components/themes/Themes";
+import dynamic from "next/dynamic";
+
+const DynamicHeader = dynamic(() => import("@/src/components/themes/Themes"), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <Themes />
+        <DynamicHeader />
         {children}
       </body>
     </html>
